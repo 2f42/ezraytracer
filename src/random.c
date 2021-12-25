a557346907c3d4c58466d8fe4c18a3bf3f0fe049
baconsenpaii <baconforevery2@gmail.com>
2021-12-25 23:54:39 +0000

    cosmetic changes

--- src/random.c
+++ src/random.c
@@ -4,21 +4,29 @@
 
 #include "random.h"
 
+
+/*
+
+TODO: make this threadsafe
+
+*/
+
+
 static uint64_t x;      // splitmix state
 static uint64_t s[4];   // xoshiro256** state
 
-static inline uint64_t rotl (const uint64_t x, int k) {
+static inline uint64_t rotl(const uint64_t x, int k) {
 	return (x << k) | (x >> (64 - k));
 }
 
-uint64_t splitmix64 () {
+uint64_t splitmix64(void) {
 	uint64_t z = (x += 0x9e3779b97f4a7c15);
 	z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9;
 	z = (z ^ (z >> 27)) * 0x94d049bb133111eb;
 	return z ^ (z >> 31);
 }
 
-void random_seed (uint64_t seed) {
+void random_seed(uint64_t seed) {
     x = seed;
     s[0] = splitmix64();
     s[1] = splitmix64();
@@ -26,7 +34,7 @@ void random_seed (uint64_t seed) {
     s[3] = splitmix64();
 }
 
-uint64_t ranl (void) {
+uint64_t ranl(void) {
 	const uint64_t result = rotl(s[1] * 5, 7) * 9;
 
 	const uint64_t t = s[1] << 17;
@@ -43,7 +51,7 @@ uint64_t ranl (void) {
 	return result;
 }
 
-double ranf (void) {
+double ranf(void) {
     union {
         unsigned long i;
         double f;

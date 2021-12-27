89f9b2ffcb6fe054cbdd48addfebc3cbb809140d
baconsenpaii <baconforevery2@gmail.com>
2021-12-27 22:59:04 +0000

    clear main

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,6 @@
 #include <omp.h>
 #include "random.h"
 
-int main(int argc, char **argv) {
-
-    #pragma omp parallel num_threads(8)
-    {
-        if (argc != 2) {
-            random_seed(12367, omp_get_thread_num());
-        } else {
-            random_seed(atol(argv[1]), omp_get_thread_num());
-        }
-    
-        printf("%F, %F, %F\n", ranf(), ranf(), ranf());
-    }
-
+int main(void) {
     return 0;
 }

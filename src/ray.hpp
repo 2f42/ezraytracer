684cef7a21eb6faf87d9ce8fcf43efe8d92422cc
baconsenpaii <baconforevery2@gmail.com>
2021-12-29 02:13:45 +0000

    remove indentation in namespace

--- src/ray.hpp
+++ src/ray.hpp
@@ -4,16 +4,16 @@
 
 namespace rt {
 
-    typedef struct {
-        glm::vec3 origin;
-        glm::vec3 direction;
-    } ray;
+typedef struct {
+    glm::vec3 origin;
+    glm::vec3 direction;
+} ray;
 
-    typedef struct {
-        glm::vec3 position;
-        glm::vec3 normal;
-        glm::vec3 incident;
-        float distance;
-    } hit;
+typedef struct {
+    glm::vec3 position;
+    glm::vec3 normal;
+    glm::vec3 incident;
+    float distance;
+} hit;
 
 }
